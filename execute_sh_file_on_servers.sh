efddc1320934040581f8280e2b91cedf2668fdcc
Marius Sterling <19165557+mariussterling@users.noreply.github.com>
2021-10-13 22:38:11 +0200

    execute script can deal with commands and files containing commands

--- execute_sh_file_on_servers.sh
+++ execute_sh_file_on_servers.sh
@@ -1,5 +1,11 @@
-while read ip
-do 
-	ssh -i acg_aws.pem ubuntu@$ip 'bash -s' < $1
-	echo $ip
-done < aws_servers.txt
+if [ -f "$1" ]; then
+	while read ip
+	do 
+		ssh -i acg_aws.pem ubuntu@$ip 'bash -s' < $1
+	done < aws_servers.txt
+else
+	while read ip
+	do
+		ssh -i acg_aws.pem ubuntu@$ip $1
+	done < aws_servers.txt
+fi
