c48b178a69387c381c3516be1c8a1b0b98cec96c
Marius Sterling <19165557+mariussterling@users.noreply.github.com>
2021-10-13 13:24:55 +0200

    add first scripts to check connection to servers and install kafka

---
+++ 1_install_kafka.sh
@@ -0,0 +1,4 @@
+wget -qO - https://packages.confluent.io/deb/6.2/archive.key | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/6.2 stable main"
+# sudo apt-get update && sudo apt-get install -y openjdk-8-jdk confluent-community-2.12
+sudo apt-get update && sudo apt-get install -y openjdk-8-jdk confluent-community-2.13
